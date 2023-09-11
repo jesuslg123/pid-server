24dd1f9bd749eac264fd310320c68c146bae0b0f
Jesus Lopez Garcia <jesus@jesuss-macbook-air.home>
2023-09-11 20:49:05 +0200

    update host

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import routes
 import settings
 
 @click.command()
-@click.option('-H', '--host', default='localhost', help='TCP/IP hostname to serve on')
+@click.option('-H', '--host', default='0.0.0.0', help='TCP/IP hostname to serve on')
 @click.option('-P', '--port', default=lambda: os.environ.get("PORT", "9099"), help='TCP/IP port to serve on')
 def run_server(host, port):
     app = web.Application(client_max_size=16*1024*1024)
